e2f73a082b97c4a19eea005fa9fa828baa61d463
Rich-10-afk <richiemwangi19@gmail.com>
2025-06-20 13:32:35 +0300

    implemented module 1: Circular queue and job management

--- modules/queue.py
+++ modules/queue.py
@@ -1,9 +1,55 @@
 from job import Job
 
+#class CircularQueue:
 class CircularQueue:
-    def __init__(self): ...
-    def enqueue(self, job: Job): ...
-    def dequeue(self): ...
-    def get_all_jobs(self) -> list: ...
-    def is_full(self) -> bool: ...
-    def is_empty(self) -> bool: ...
+        Default_Capacity = 10
+
+        def __init__(self):
+            self._data = [None] * CircularQueue.Default_Capacity
+            self._size = 0
+            self._front = 0
+
+        def __len__(self):
+            return self._size
+
+        def is_empty(self):
+            return self._size == 0
+
+        def first(self):
+            if self.is_empty():
+                raise IndexError("Queue empty")
+            return self._data[self._front]
+
+        def dequeue(self):
+            if self.is_empty():
+                raise IndexError("Queue is empty")
+            answer = self._data[self._front]
+            self._data[self._front] = None
+            self._front = (self._front + 1) % len(self._data)
+            self._size -= 1
+            return answer
+
+        def enqueue(self, e):
+            if self._size == len(self._data):
+                self.resize(2 * len(self._data))
+            avail = (self._front + self._size) % len(self._data)
+            self._data[avail] = e
+            self._size += 1
+
+        def resize(self, cap):
+            old = self._data
+            self._data = [None] * cap
+            walk = self._front
+            for k in range(self._size):
+                self._data[k] = old[walk]
+                walk = (1 + walk) % len(old)
+            self._front = 0
+
+
+
+    # def __init__(self): ...
+    # def enqueue(self, job: Job): ...
+    # def dequeue(self): ...
+    # def get_all_jobs(self) -> list: ...
+    # def is_full(self) -> bool: ...
+    # def is_empty(self) -> bool: ...
--- queue_manager.py
+++ queue_manager.py
@@ -1,55 +1,10 @@
 from job import Job
+from modules.queue import CircularQueue
 from modules.priority_aging import PriorityManager
 from modules.job_expiry import JobExpiryManager
 from modules.concurrent_submission import SubmissionManager
 from modules.event_simulator import TickSimulator
 from modules.visualization import Visualizer
-
-class CircularQueue:
-    Default_Capacity = 10
-
-    def __init__(self):
-        self._data = [None] * CircularQueue.Default_Capacity
-        self._size = 0
-        self._front = 0
-
-    def __len__(self):
-        return self._size
-
-    def is_empty(self):
-        return self._size == 0
-
-    def first(self):
-        if self.is_empty():
-            raise IndexError("Queue empty")
-        return self._data[self._front]
-
-    def dequeue(self):
-        if self.is_empty():
-            raise IndexError("Queue is empty")
-        answer = self._data[self._front]
-        self._data[self._front] = None
-        self._front = (self._front + 1) % len(self._data)
-        self._size -= 1
-        return answer
-
-    def enqueue(self, e):
-        if self._size == len(self._data):
-            self.resize(2 * len(self._data))
-        avail = (self._front + self._size) % len(self._data)
-        self._data[avail] = e
-        self._size += 1
-
-    def resize(self, cap):
-        old = self._data
-        self._data = [None] * cap
-        walk = self._front
-        for k in range(self._size):
-            self._data[k] = old[walk]
-            walk = (1 + walk) % len(old)
-        self._front = 0
-
-
 
 class PrintQueueManager:
     def __init__(self):
