38659e5f0635ffd3800235a5a8ef754cc0b4f918
ekangethe-dev <kangetheernest@gmail.com>
2025-06-23 08:35:43 +0300

    Implemented Priority and Aging System

--- modules/priority_aging.py
+++ modules/priority_aging.py
@@ -1,4 +1,18 @@
 class PriorityManager:
-    def __init__(self, queue): ...
-    def apply_priority_aging(self, current_tick: int): ...
-    def reorder_queue(self): ...
+    def __init__(self, queue, aging_threshold=5):
+        self.queue = queue
+        self.aging_threshold = aging_threshold
+
+    def apply_priority_aging(self, current_tick: int):
+        for job in self.queue.items:
+            job["waiting_time"] = current_tick - job["arrival_tick"]
+
+            if job["waiting_time"] > self.aging_threshold:
+                job["priority"] += 1
+
+        self.reorder_queue()
+
+    def reorder_queue(self):
+        self.queue.items.sort(
+            key=lambda job: (-job["priority"], -job["waiting_time"])
+        )
