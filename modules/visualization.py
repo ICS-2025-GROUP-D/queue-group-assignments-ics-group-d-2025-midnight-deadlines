f2190ed161cb8fd7807adbf475da9da17097632e
aikoa <aikoangie02@gmail.com>
2025-07-03 10:33:21 +0300

    resolve merge conflicts in visualization.py

--- modules/visualization.py
+++ modules/visualization.py
@@ -1,3 +1,20 @@
 class Visualizer:
-    def __init__(self, queue): ...
-    def display_queue(self): ...
+    def _init_(self, queue):
+        self.queue = queue
+
+    def display_queue(self):
+        if self.queue.is_empty():
+            print("\nQueue is empty.\n")
+            return
+
+        print("\nCurrent Print Queue:")
+        print("{:<10} {:<10} {:<10} {:<10}".format("UserID", "JobID", "Priority", "Time"))
+        print("-" * 45)
+
+        jobs = self.queue.get_all_jobs()
+
+        for job in jobs:
+            print("{:<10} {:<10} {:<10} {:<10}".format(
+                job.user_id, job.job_id, job.priority, job.created_at
+            ))
+        print()
